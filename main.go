84c77e053ea1f815da712f652a7fdda18b98ed6e
Jacob Winn <jmwinn21@gmail.com>
2018-12-11 11:23:56 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+type WordsResponse struct {
+	Words []string `json:"words"`
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	currentWords := WordsResponse{
+		Words: []string{
+			"test1",
+			"test2",
+		},
+	}
+
+	router.GET("/current", func(c *gin.Context) {
+		c.JSON(http.StatusOK, currentWords)
+	})
+
+	router.Run(":" + port)
+}
